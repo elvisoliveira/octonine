113057992e7774c057d20c59e1da5f551f8199a7
Elvis Oliveira <elvis.olv@gmail.com>
2014-04-24 23:47:04 -0300

    netbeans config and make files cURL implemented with parser working .gitignore corrected

--- main.cpp
+++ main.cpp
@@ -1,53 +1,75 @@
-#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <map>
+
 #include "rapidxml-1.13/rapidxml.hpp"
 #include "rapidxml-1.13/rapidxml_utils.hpp"
 
-using namespace std;
+#include <curl/curl.h>
 
-int main() {
+size_t write_to_string(void * ptr, size_t size, size_t count, void * stream)
+{
+    ((std::string *) stream)->append((char *) ptr, 0, size * count);
+    return size * count;
+}
 
-    std::string input_xml;
-    std::string line;
+int main(void)
+{
 
-    ifstream in("C:/Users/elvis.nunes/Desktop/feed.xml");
+    CURL * curl = curl_easy_init();
+    CURLcode res;
 
-    // read file into input_xml
-    while (getline(in, line))
-        input_xml += line;
+    // the "response" variable should now contain the contents of the HTTP response
+    std::string response;
 
-    // make a safe-to-modify copy of input_xml
-    // (you should never modify the contents of an std::string directly)
-    vector<char> buffer(input_xml.begin(), input_xml.end());
+    if (curl)
+    {
+        // init cURL params
+        curl_easy_setopt(curl, CURLOPT_URL, "http://9gagrss.com/feed/");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, & response);
 
-    buffer.push_back('\0');
+        // 
+        res = curl_easy_perform(curl);
 
-    // only use xml_copy from here on!
-    rapidxml::xml_document<> doc;
-
-    doc.parse<0>(&buffer[0]);
-
-    // posts
-    
-   
-    std::vector<std::map<std::string, std::string>> teste;
-
-    // first level
-    for (rapidxml::xml_node<> * attr = doc.first_node("rss")->first_node("channel")->first_node("item"); attr; attr = attr->next_sibling()) {
+        // clean cURL request
+        curl_easy_cleanup(curl);
 
 
-        
-        m["link"] = attr->first_node("link")->value();
-        m["title"] = attr->first_node("title")->first_node()->value();
-        m["pubDate"] = attr->first_node("pubDate")->value();
-        m["description"] = attr->first_node("description")->first_node()->value();
-        m["guid"] = attr->first_node("guid")->value();
 
-        v.push_back(m);
+        std::string input_xml;
+        std::string line;
+
+        // only use xml_copy from here on!
+        rapidxml::xml_document<> doc;
+
+
+        char * cstr = new char[response.size() + 1];
+
+        strcpy(cstr, response.c_str());
+
+        doc.parse<0>(cstr);
+
+
+        // first level
+        for (rapidxml::xml_node<> * attr = doc.first_node("rss")->first_node("channel")->first_node("item"); attr; attr = attr->next_sibling())
+        {
+
+            //        attr->first_node("link")->value();
+            //        attr->first_node("title")->first_node()->value();
+            //        attr->first_node("pubDate")->value();
+            //        attr->first_node("description")->first_node()->value();
+            //        attr->first_node("guid")->value();
+
+            std::cout << attr->first_node("link")->value();
+
+        }
 
     }
 
     return 0;
-}
+
+}	

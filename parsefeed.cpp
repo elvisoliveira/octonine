1a15fe1e9c9339d23a670d98da0e4131cb589029
Elvis Oliveira <elvis.olv@gmail.com>
2014-05-09 10:57:02 -0300

    ignoring more netbeans hidden files. integration with octopp. first version of displaying data for UI

--- parsefeed.cpp
+++ parsefeed.cpp
@@ -4,48 +4,47 @@
 #include "parsefeed.h"
 
 parsefeed::parsefeed(const std::string & document) {
-	// convert returned document string in char
-	char * cstr = new char[document.size() + 1];
-	
-	// strcpy only works with C style char
-    std::strcpy(cstr, document.c_str());
-	
-	// initialize the rapidxml files
-	rapidxml::xml_document<> doc;
+    // convert returned document string in char
+    char * cstr = new char[document.size() + 1];
 
-	// parse the char using rapidXML
+    // strcpy only works with C style char
+    std::strcpy(cstr, document.c_str());
+
+    // initialize the rapidxml files
+    rapidxml::xml_document<> doc;
+
+    // parse the char using rapidXML
     doc.parse<0>(cstr);
 
-	std::vector <std::map<std::string, std::string> > example;
+    std::vector <std::map<std::string, std::string> > example;
 
     // loop feed itens
-    for (rapidxml::xml_node<> * attr = doc.first_node("rss")->first_node("channel")->first_node("item"); attr; attr = attr->next_sibling()) {
+    for (rapidxml::xml_node<> * attr = doc.first_node("articles")->first_node("article"); attr; attr = attr->next_sibling()) {
 
-		// create the basic hashmap
-		std::map<std::string, std::string> postMap;
+        // create the basic hashmap
+        std::map<std::string, std::string> postMap;
 
-		// convert to C style hashmap
-		postMap["link"] = attr->first_node("link")->value();
-		postMap["title"] = attr->first_node("title")->first_node()->value();
-		postMap["pubDate"] = attr->first_node("pubDate")->value();
-		postMap["description"] = attr->first_node("description")->first_node()->value();
-		postMap["guid"] = attr->first_node("guid")->value();
+        // convert to C style hashmap
+        postMap["points"] = attr->first_node("points")->value();
+        postMap["comments"] = attr->first_node("comments")->value();
+        postMap["id"] = attr->first_node("id")->value();
+        postMap["next"] = attr->first_node("next")->value();
+        postMap["content"] = attr->first_node("content")->value();
+        postMap["title"] = attr->first_node("title")->value();
 
-		// push hashmap to the end of vector
-		example.push_back(postMap);
+        // push hashmap to the end of vector
+        example.push_back(postMap);
 
     }
 
-	for(int i = 0; i < example.size(); i++) {
+    for (int i = 0; i < example.size(); i++) {
 
-		std::cout << example.at(i)["link"] << std::endl;
-		std::cout << example.at(i)["title"] << std::endl;
-		std::cout << example.at(i)["pubDate"] << std::endl;
-		std::cout << example.at(i)["description"] << std::endl;
-		std::cout << example.at(i)["guid"] << std::endl;
-		std::cout << std::endl;
+        std::cout << "[" << i << "]" << "[points]" << example.at(i)["points"] << std::endl;
+        std::cout << "[" << i << "]" << "[comments]" << example.at(i)["comments"] << std::endl;
+        std::cout << "[" << i << "]" << "[id]" << example.at(i)["id"] << std::endl;
+        std::cout << "[" << i << "]" << "[next]" << example.at(i)["next"] << std::endl;
+        std::cout << "[" << i << "]" << "[content]" << example.at(i)["content"] << std::endl;
+        std::cout << "[" << i << "]" << "[title]" << example.at(i)["title"] << std::endl;
 
-	}
-
-	
+    }
 }
